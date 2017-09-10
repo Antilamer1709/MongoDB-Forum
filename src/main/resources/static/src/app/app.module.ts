3a41ad3c71085e3e8c42a3b8279f48c191e372ba
vsloboda <vsloboda>
2017-09-10 02:22:15 +0200

    initial commit

--- src/main/resources/static/src/app/app.module.ts
+++ src/main/resources/static/src/app/app.module.ts
@@ -2,15 +2,25 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import {HttpModule} from "@angular/http";
+import {routes} from "./app.routes";
+import {RouterModule} from "@angular/router";
+import {DashboardService} from "./dashboard/dashboard.service";
+import {DashboardComponent} from "./dashboard/dashboard.component";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DashboardComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpModule,
+    RouterModule.forRoot( routes ),
   ],
-  providers: [],
+  providers: [
+    DashboardService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
