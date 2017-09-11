fbe5746643b6ea33012f1e7e67e4486fe70ab137
vsloboda <vsloboda>
2017-09-11 02:23:15 +0200

    security config

--- src/main/java/com/antilamer/mongoDbForum/config/WebSecurityConfig.java
+++ src/main/java/com/antilamer/mongoDbForum/config/WebSecurityConfig.java
@@ -1,30 +1,46 @@
 package com.antilamer.mongoDbForum.config;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-
-import javax.sql.DataSource;
 
 @Configuration
 @EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
+    public void configure(WebSecurity web) throws Exception {
+        web.ignoring().
+                antMatchers(HttpMethod.OPTIONS, "/**").
+                antMatchers("/").
+                antMatchers("/*.{js,html}").
+                antMatchers("/img/**").
+                antMatchers("/fonts/**").
+                antMatchers("/css/**").
+                antMatchers("/404.html").
+                antMatchers("/js/**").
+                antMatchers("/node_modules/**").
+                antMatchers("/**/*.{js,html,css}");
+
+        web.ignoring().antMatchers( "/", "/resources/**", "/index.*", "/login.html","/favicon.ico",
+                "/template/**", "/assets", "/assets/**", "/node_modules", "/node_modules/**", "/dist", "/dist/**",
+                "/*.ttf", "/*.woff2", "/dashboard", "/dashboard/**", "/authentication/**", "/authentication/login",
+                "/authentication/registration");
+    }
+
+    @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .authorizeRequests()
-                .antMatchers("/**", "/registration", "/javax.faces.resource/**", "/login").permitAll()
-                .antMatchers("/admin/**").hasAuthority("ADMIN").anyRequest()
-                .authenticated()
+                .antMatchers("/authentication/**").permitAll()
+                .antMatchers("/index").permitAll()
+                .anyRequest().authenticated()
                 .and()
                 .formLogin()
-                .loginPage("/login").failureUrl("/login?error=true")
+                .loginPage("/authentication/login")
                 .defaultSuccessUrl("/")
                 .usernameParameter("username").passwordParameter("password")
                 .permitAll()
